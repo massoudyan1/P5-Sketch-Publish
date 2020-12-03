97090b2124ff4bd37e3b0124f61f3849cce90000
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2020-12-03 14:34:28 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,23 +3,62 @@ var bird;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+  ellipseMode(CENTER);
   bird = new Bird();
   }
 
 function draw() {
-  background(220);
-  rect(width/2,height/2,width/2,height/2);
+  background(20);
+  
+  bird.update();
   bird.show();
+
+  
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    bird.up();
+  }
 }
 
 function Bird(){
-  this.x = width/16;
+  this.x = width/10;
   this.y = height/2;
-
+  
+  this.gravity = 0.5;
+  this.vel= 0;
+  
+  this.size = width/15;
+    
+  
+  this.up = function(){
+    this.vel += -this.gravity*70;
+  }
+  
   this.show = function(){
     fill(255);
-    ellipse(this.x, this.y, width/16, width/16);
+    ellipse(this.x, this.y, this.size, width/15);
   }
 
+  this.update = function(){
+    this.vel += this.gravity;
+    this.y += this.vel;
+    
+    if(this.vel > 8){
+      this.vel = 8;
+    }else if(this.vel < -8){
+      this.vel = -8;
+    }
+    
+   if(this.y > height-this.size/2){
+      this.y = height-this.size/2;
+      this.vel = 0;
+    }
+    if(this.y < this.size/2){
+      this.y = this.size/2;
+    }
+    
+  } 
 
 }
