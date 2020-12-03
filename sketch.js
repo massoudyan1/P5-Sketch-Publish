7d178cd77dd1bf0e8e57936ac9b999adb57e7de8
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2020-12-03 14:27:17 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
+var bird;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+  bird = new Bird();
   }
 
 function draw() {
   background(220);
   rect(width/2,height/2,width/2,height/2);
+  bird.show();
+}
+
+function Bird(){
+  this.x = width/16;
+  this.y = height/2;
+
+  this.show = function(){
+    fill(255);
+    ellipse(this.x, this.y, width/16, width/16);
+  }
+
+
 }
