796b8650ff656df17a1841a262da26daa29a1d19
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2020-12-03 10:46:32 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
+  rectMode(CENTER);
   }
-  
-  function draw() {
-    background(220);
-  }
+
+function draw() {
+  background(220);
+  rect(width/2,height/2,width/2,height/2);
+}
